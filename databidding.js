ee4ad4a986a0112505cf8994334dd4b105b50b2c
Nguyễn Minh Tuấn <admin@adminnoMacBook-Pro.local>
2026-01-19 00:51:16 +0700

    update func

--- databidding.js
+++ databidding.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carousel = document.getElementById("carouselVehicle");
+  const carousel_mbike = document.getElementById("carouselVehicle");
 
-  const speedEl = document.getElementById("speed");
-  const distanceEl = document.getElementById("distance");
-  const truckEl = document.getElementById("bike_truck");
-  const priceEl = document.getElementById("price");
+  const speed_mbike = document.getElementById("speed");
+  const distance_mbike = document.getElementById("distance");
+  const truck_mbike = document.getElementById("bike_truck");
+  const price_mbike = document.getElementById("price");
 
   function updateInfo(index) {
     const v = vehicles[index];
     if (!v) return;
 
-    speedEl.innerText = v.speed;
-    distanceEl.innerText = v.distance;
-    truckEl.innerText = v.truck;
-    priceEl.innerText = v.price;
+    speed_mbike.innerText = v.speed;
+    distance_mbike.innerText = v.distance;
+    truck_mbike.innerText = v.truck;
+    price_mbike.innerText = v.price;
   }
 
   // load xe đầu tiên
   updateInfo(0);
 
   // khi slide xong
-  carousel.addEventListener("slid.bs.carousel", (e) => {
+  carousel_mbike.addEventListener("slid.bs.carousel", (e) => {
     updateInfo(e.to);
   });
 });
